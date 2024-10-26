d7a6e6d3e067e395bae9b94411ecded62b62af8b
Aman5685 <amanyadav5685@gmail.com>
2024-10-26 12:43:25 +0530

    Done

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// jsx-means it is a combination of HTML and javascript, 
+function Home() {
+  return (
+   <>
+   <h1>This is my first heading</h1>
+   <p id="para1">This is a paragraph</p>
+   <h2 className="heading">This is a Second Heading</h2>
+   </>
+  )
+}
+
+export default Home
